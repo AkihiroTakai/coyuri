fbaf25b91c5b446ff81c8d8f4cfdfb9dfd0b1820
Akihiro Takai <at.sisy@gmail.com>
2016-10-16 19:56:00 +0900

    盤面保持をstd::vectorからただの配列に変更

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,20 @@
 #include <FL/Fl_PNG_Image.H>
 
 const int IMAGE_SIDE = 45;
+Fl_Box *ban[9][9];
 
 int main(int argc, char **argv){
 
 	fl_register_images();
-
 	Fl_Window win(610, 700, "Coyuri");
-	std::vector<std::vector<Fl_Box *> > ban;
 
 	Fl_PNG_Image img("/home/takai/Pictures/coyuri/ban.png");
 
-	for(int y = 0;y < 9;y++){
-		ban.push_back(std::vector<Fl_Box *>());
+      for(int y = 0;y < 9;y++){
 		for(int x = 0;x < 9;x++){
 			Fl_Box *fl_box = new Fl_Box(x*IMAGE_SIDE, y*IMAGE_SIDE, x*IMAGE_SIDE+70, y*IMAGE_SIDE+70);
 			fl_box->image(img);
-			ban.back().push_back(fl_box);
+			ban[x][y] = fl_box;
 		}
 	}
 
