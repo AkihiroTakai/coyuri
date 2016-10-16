37ff9f886a147ff176787c44c8a116f6aeb5b170
Akihiro Takai <at.sisy@gmail.com>
2016-10-16 20:31:36 +0900

    駒を任意の位置へ配置できるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 #include <FL/Fl_PNG_Image.H>
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Button.H>
+#include <string>
 
 const int IMAGE_SIDE = 45;
 Fl_Box *ban[9][9];
 
-void draw_koma(Fl_Widget* widget);
+void draw_koma(Fl_Widget* widget, void *v);
+int ctoi(char ch);
 
 int main(int argc, char **argv){
 
@@ -27,9 +29,11 @@ int main(int argc, char **argv){
 		}
 	}
 
-	Fl_Input* input = new Fl_Input(200, 640, 70, 30, "手:");
-	Fl_Button* button = new Fl_Button(300, 630, 50, 50, "打つ");
-	button->callback(draw_koma);
+	Fl_Input *input = new Fl_Input(200, 640, 70, 30, "手:");
+	Fl_Button *button = new Fl_Button(300, 630, 50, 50, "打つ");
+
+	void* v[] = {input};
+	button->callback(draw_koma, v);
 
 	win.end();
 	win.show(argc, argv);
@@ -37,7 +41,22 @@ int main(int argc, char **argv){
 	return Fl::run();
 }
 
-void draw_koma(Fl_Widget* widget){
-	ban[7][7]->image(new Fl_PNG_Image("/home/takai/Pictures/coyuri/kyousha.png"));
-	ban[7][7]->redraw();
+/*
+ *駒を描画する関数
+ */
+void draw_koma(Fl_Widget* widget, void *v){
+      std::string input = ((Fl_Input*)((void**)v)[0])->value();
+	int x, y;
+	x = ctoi(input.c_str()[0]);
+	y = ctoi(input.c_str()[1]);
+	ban[x-1][y-1]->image(new Fl_PNG_Image("/home/takai/Pictures/coyuri/kyousha.png"));
+	ban[x-1][y-1]->redraw();
+}
+
+/*
+ *charからintへ変換
+ */
+int ctoi(char ch){
+	if('0' <= ch && ch <= '9') return (ch-'0');
+	else return -1;
 }
