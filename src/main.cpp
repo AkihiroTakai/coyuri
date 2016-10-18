15cc90bb7213ff11c8a83a2390c69bc1e121822f
Akihiro Takai <at.sisy@gmail.com>
2016-10-18 12:27:30 +0900

    行と列を表示するようにした

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,21 @@ Fl_PNG_Image *images[17];
 int main(int argc, char **argv){
 
 	fl_register_images();
-	Fl_Window win(610, 700, "Coyuri");
+	Fl_Window win(710, 800, "Coyuri");
 
-      for(int y = 0;y < 9;y++){
-		for(int x = 0;x < 9;x++){
+      for(int y = 1;y < 10;y++){
+		for(int x = 1;x < 10;x++){
 			Fl_Box *fl_box = new Fl_Box(x*IMAGE_SIDE, y*IMAGE_SIDE, x*IMAGE_SIDE+70, y*IMAGE_SIDE+70);
 			fl_box->image(NULL);
-			ban[x][y] = fl_box;
+			ban[x-1][y-1] = fl_box;
 		}
 	}
 
-	Fl_Input *input = new Fl_Input(200, 640, 70, 30, "手:");
-	Fl_Button *button = new Fl_Button(300, 630, 50, 50, "打つ");
+	Fl_Box *gyou = new Fl_Box(100, 20, 550, 65, "九        　八　          七　           六            五            四          　三            　二　           一");
+	Fl_Box *suji = new Fl_Box(640, 70, 110, 610, "1\n\n\n\n\n2\n\n\n\n\n3\n\n\n\n\n4\n\n\n\n\n5\n\n\n\n\n6\n\n\n\n\n7\n\n\n\n\n8\n\n\n\n\n9");
+
+	Fl_Input *input = new Fl_Input(250, 740, 70, 30, "手:");
+	Fl_Button *button = new Fl_Button(350, 730, 50, 50, "打つ");
 
 	void* v[] = {input};
 	button->callback(draw_koma, v);
