46809533c03fedb9f381739bf47244614bb34e92
Akihiro Takai <at.sisy@gmail.com>
2016-10-18 12:30:53 +0900

    行と列の添字を表示する処理をinit関数に移動した

--- src/init.cpp
+++ src/init.cpp
@@ -84,4 +84,6 @@ void put_koma_init(){
 void init(){
 	images_path_init();
 	put_koma_init();
+	Fl_Box *gyou = new Fl_Box(100, 20, 550, 65, "九        　八　          七　           六            五            四          　三            　二　           一");
+	Fl_Box *suji = new Fl_Box(640, 70, 110, 610, "1\n\n\n\n\n2\n\n\n\n\n3\n\n\n\n\n4\n\n\n\n\n5\n\n\n\n\n6\n\n\n\n\n7\n\n\n\n\n8\n\n\n\n\n9");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,6 @@ int main(int argc, char **argv){
 		}
 	}
 
-	Fl_Box *gyou = new Fl_Box(100, 20, 550, 65, "九        　八　          七　           六            五            四          　三            　二　           一");
-	Fl_Box *suji = new Fl_Box(640, 70, 110, 610, "1\n\n\n\n\n2\n\n\n\n\n3\n\n\n\n\n4\n\n\n\n\n5\n\n\n\n\n6\n\n\n\n\n7\n\n\n\n\n8\n\n\n\n\n9");
-
 	Fl_Input *input = new Fl_Input(250, 740, 70, 30, "手:");
 	Fl_Button *button = new Fl_Button(350, 730, 50, 50, "打つ");
 
