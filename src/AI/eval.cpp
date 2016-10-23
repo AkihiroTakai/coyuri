17069f764b1a3a9becc78e9b53bc3565d50288bb
Akihiro Takai <at.sisy@gmail.com>
2016-10-23 16:29:52 +0900

    評価関数に２つ新しく評価基準を追加

--- src/AI/eval.cpp
+++ src/AI/eval.cpp
@@ -4,13 +4,16 @@
 #include <cmath>
 #include <iostream>
 
-const int WITHIN_OU    = 5;
-const int WITHIN_HISHA = 3;
-const int WITHIN_KAKU  = 2;
+const int WITHIN_OU     = 5;
+const int WITHIN_HISHA  = 3;
+const int WITHIN_KAKU   = 2;
+const int WITHIN_PLAYER = -15;
 
 int within_ou(BANMEN *banmen);
 int within_hisha(BANMEN *banmen);
 int within_kaku(BANMEN *banmen);
+int within_player(BANMEN *banmen);
+int num_on_ban(BANMEN *banmen);
 /*
  *注
  *ここで言う"敵"とはプレイヤーのこと
@@ -37,6 +40,16 @@ int EVAL(Node *node){
 	 *角が自分の陣地の外にいればマイナス
 	 */
 	score += within_kaku(node->get_banmen());
+
+	/*
+	 *自分の陣地にプレーヤーの駒が入ってくる場合
+	 */
+	score += within_player(node->get_banmen());
+
+	/*
+	 *自分の駒が盤面に何枚残っているか
+	 */
+	score += num_on_ban(node->get_banmen());
 
 	return score;
 }
@@ -65,6 +78,30 @@ int within_kaku(BANMEN *banmen){
 	return -WITHIN_KAKU;
 }
 
+/*
+ *自分の陣地にプレーヤーの駒が入ってくる場合
+ */
+int within_player(BANMEN *banmen){
+	for(int y = 0;y < 3;y++)
+		for(int x = 0;x < 9;x++)
+			if(banmen->get_type(x, y) >= HU && banmen->get_type(x, y) <= OU)
+				return WITHIN_PLAYER;
+
+	return -WITHIN_PLAYER;
+}
+
+/*
+ *自分の陣地にプレーヤーの駒が入ってくる場合
+ */
+int num_on_ban(BANMEN *banmen){
+	int count;
+	for(int y = 0;y < 9;y++)
+		for(int x = 0;x < 9;x++)
+			if(banmen->get_type(x, y) >= EN_HU && banmen->get_type(x, y) <= EN_OU)
+			     count++;
+
+	return count*2;
+}
 
 /*
  *渡された盤面からコンピュータがさせる手をすべてリストアップする関数
@@ -75,7 +112,7 @@ void EXPAND(Node *node){
 		for(int y = 0;y < 9;y++){
 			if(node->get_banmen()->get_type(x, y) >= EN_HU && node->get_banmen()->get_type(x, y) <= EN_OU){
 				for(Point p : wcm_ftable[node->get_banmen()->get_type(x, y)](Point(std::abs(x-9), y+1))){
-				      BANMEN *new_banmen = new BANMEN;
+					BANMEN *new_banmen = new BANMEN;
 					new_banmen->copy_banmen(node->get_banmen());
 					new_banmen->set_type(9-p.get_x(), p.get_y()-1, node->get_banmen()->get_type(x, y));
 					new_banmen->set_type(x, y, EMPTY);
