734841d4baf90679314f93bafb85d38f229722cd
Akihiro Takai <at.sisy@gmail.com>
2016-10-25 19:42:10 +0900

    寝返り処理がうまく行かないバグを修正

--- src/move.cpp
+++ src/move.cpp
@@ -34,7 +34,7 @@ void move(Point replace){
 
 	if(main_ban[9-replace.get_x()][replace.get_y()-1] != EMPTY){
 		//手駒を取れたら
-		PLAYER_TEGOMA.push_back(main_ban[9-replace.get_x()][replace.get_y()-1]);
+		PLAYER_TEGOMA.push_back(negaeri(main_ban[9-replace.get_x()][replace.get_y()-1]));
 	}
 	set_and_redraw(Point(replace.get_x(), replace.get_y()), main_ban[TARGET_KOMA.get_x()][TARGET_KOMA.get_y()]);
 	set_and_redraw(Point(9-TARGET_KOMA.get_x(), TARGET_KOMA.get_y()+1), EMPTY);
